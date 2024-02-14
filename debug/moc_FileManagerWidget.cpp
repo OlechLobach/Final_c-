e90b438dc792476c99a8c6003e67832c7840f417
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-02-14 23:34:12 +0200

    init

---
+++ debug/moc_FileManagerWidget.cpp
@@ -0,0 +1,312 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'FileManagerWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../managerF/FileManagerWidget.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'FileManagerWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS = QtMocHelpers::stringData(
+    "FileManagerWidget",
+    "openDrive",
+    "",
+    "index",
+    "goBack",
+    "addToHistory",
+    "path",
+    "goForward",
+    "openDirectory",
+    "QModelIndex",
+    "createFileOrFolder",
+    "showContextMenu",
+    "pos",
+    "renameFile",
+    "deleteFileOrFolder",
+    "copyFileOrFolder",
+    "pasteFileOrFolder",
+    "createFolder",
+    "createFile",
+    "updatePath",
+    "search",
+    "text",
+    "startSearch"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS_t {
+    uint offsetsAndSizes[46];
+    char stringdata0[18];
+    char stringdata1[10];
+    char stringdata2[1];
+    char stringdata3[6];
+    char stringdata4[7];
+    char stringdata5[13];
+    char stringdata6[5];
+    char stringdata7[10];
+    char stringdata8[14];
+    char stringdata9[12];
+    char stringdata10[19];
+    char stringdata11[16];
+    char stringdata12[4];
+    char stringdata13[11];
+    char stringdata14[19];
+    char stringdata15[17];
+    char stringdata16[18];
+    char stringdata17[13];
+    char stringdata18[11];
+    char stringdata19[11];
+    char stringdata20[7];
+    char stringdata21[5];
+    char stringdata22[12];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS_t qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 17),  // "FileManagerWidget"
+        QT_MOC_LITERAL(18, 9),  // "openDrive"
+        QT_MOC_LITERAL(28, 0),  // ""
+        QT_MOC_LITERAL(29, 5),  // "index"
+        QT_MOC_LITERAL(35, 6),  // "goBack"
+        QT_MOC_LITERAL(42, 12),  // "addToHistory"
+        QT_MOC_LITERAL(55, 4),  // "path"
+        QT_MOC_LITERAL(60, 9),  // "goForward"
+        QT_MOC_LITERAL(70, 13),  // "openDirectory"
+        QT_MOC_LITERAL(84, 11),  // "QModelIndex"
+        QT_MOC_LITERAL(96, 18),  // "createFileOrFolder"
+        QT_MOC_LITERAL(115, 15),  // "showContextMenu"
+        QT_MOC_LITERAL(131, 3),  // "pos"
+        QT_MOC_LITERAL(135, 10),  // "renameFile"
+        QT_MOC_LITERAL(146, 18),  // "deleteFileOrFolder"
+        QT_MOC_LITERAL(165, 16),  // "copyFileOrFolder"
+        QT_MOC_LITERAL(182, 17),  // "pasteFileOrFolder"
+        QT_MOC_LITERAL(200, 12),  // "createFolder"
+        QT_MOC_LITERAL(213, 10),  // "createFile"
+        QT_MOC_LITERAL(224, 10),  // "updatePath"
+        QT_MOC_LITERAL(235, 6),  // "search"
+        QT_MOC_LITERAL(242, 4),  // "text"
+        QT_MOC_LITERAL(247, 11)   // "startSearch"
+    },
+    "FileManagerWidget",
+    "openDrive",
+    "",
+    "index",
+    "goBack",
+    "addToHistory",
+    "path",
+    "goForward",
+    "openDirectory",
+    "QModelIndex",
+    "createFileOrFolder",
+    "showContextMenu",
+    "pos",
+    "renameFile",
+    "deleteFileOrFolder",
+    "copyFileOrFolder",
+    "pasteFileOrFolder",
+    "createFolder",
+    "createFile",
+    "updatePath",
+    "search",
+    "text",
+    "startSearch"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSFileManagerWidgetENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,  110,    2, 0x08,    1 /* Private */,
+       4,    0,  113,    2, 0x08,    3 /* Private */,
+       5,    1,  114,    2, 0x08,    4 /* Private */,
+       7,    0,  117,    2, 0x08,    6 /* Private */,
+       8,    1,  118,    2, 0x08,    7 /* Private */,
+      10,    1,  121,    2, 0x08,    9 /* Private */,
+      11,    1,  124,    2, 0x08,   11 /* Private */,
+      13,    1,  127,    2, 0x08,   13 /* Private */,
+      14,    1,  130,    2, 0x08,   15 /* Private */,
+      15,    1,  133,    2, 0x08,   17 /* Private */,
+      16,    1,  136,    2, 0x08,   19 /* Private */,
+      17,    0,  139,    2, 0x08,   21 /* Private */,
+      18,    0,  140,    2, 0x08,   22 /* Private */,
+      19,    0,  141,    2, 0x08,   23 /* Private */,
+      20,    1,  142,    2, 0x08,   24 /* Private */,
+      22,    0,  145,    2, 0x08,   26 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void, QMetaType::QPoint,   12,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void, 0x80000000 | 9,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   21,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject FileManagerWidget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSFileManagerWidgetENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<FileManagerWidget, std::true_type>,
+        // method 'openDrive'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'goBack'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addToHistory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        // method 'goForward'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'openDirectory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'createFileOrFolder'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'showContextMenu'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QPoint &, std::false_type>,
+        // method 'renameFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'deleteFileOrFolder'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'copyFileOrFolder'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'pasteFileOrFolder'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'createFolder'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'createFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'updatePath'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'search'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        // method 'startSearch'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void FileManagerWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<FileManagerWidget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->openDrive((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 1: _t->goBack(); break;
+        case 2: _t->addToHistory((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 3: _t->goForward(); break;
+        case 4: _t->openDirectory((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 5: _t->createFileOrFolder((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 6: _t->showContextMenu((*reinterpret_cast< std::add_pointer_t<QPoint>>(_a[1]))); break;
+        case 7: _t->renameFile((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 8: _t->deleteFileOrFolder((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 9: _t->copyFileOrFolder((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 10: _t->pasteFileOrFolder((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 11: _t->createFolder(); break;
+        case 12: _t->createFile(); break;
+        case 13: _t->updatePath(); break;
+        case 14: _t->search((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 15: _t->startSearch(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *FileManagerWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *FileManagerWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSFileManagerWidgetENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int FileManagerWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
